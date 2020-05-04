dafe5ebaa419c34abc0eca3474b598608b712649
mari_s <ms.mari.suzuki@gmail.com>
2020-05-04 22:05:15 +0900

    started 173. Add a Basket - added Basket.java

---
+++ src/academy/learnprogramming/Basket.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Basket {
+    private final String name;
+    private final Map<StockItem, Integer> list;
+
+    public Basket(String name) {
+        this.name = name;
+        this.list = new HashMap<>();
+    }
+}
